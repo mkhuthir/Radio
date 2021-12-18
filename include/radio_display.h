4a28a5b9af7947ba034a40d8cc31e0557b819ac9
mkhuthir <mkhuthir@gmail.com>
2021-12-18 20:46:03 +0400

    added vol

--- include/radio_display.h
+++ include/radio_display.h
@@ -15,6 +15,7 @@ void updateDisplay(Si4703& radio, Nextion& disp)
 {
   disp.setComponentText("rssi", String(radio.getRSSI()));
   disp.setComponentText("freq", String(float(radio.getChannel())/100,2));
+  disp.setComponentText("vol", String(radio.getVolume()));
   // TODO: Add other parameters VOL,...
 }
 
